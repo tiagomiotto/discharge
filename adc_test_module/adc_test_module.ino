f62c43ded6526012c9df54664b3425634d70091e
tiagomiotto <34488331+tiagomiotto@users.noreply.github.com>
2018-01-11 17:40:17 +0000

    Test module for the pcb
    Added two testing module, for the porpoise of testing only the voltage measuring capacities of the pcb as well as a simple discharge of a dummy cell at the maximum level of the potentiometer ie. smallest current.

---
+++ adc_test_module/adc_test_module.ino
@@ -0,0 +1,28 @@
+/*Adc testing module 10/01
+Test the adc by measuring a dummy voltage provided by the PSU
+*/
+
+#include <Adafruit_ADS1015.h>
+#include <Wire.h>
+#include <math.h> 
+
+/*Fill in the desired adc addr!*/
+Adafruit_ADS1015 ads1115_1(0x48); 
+
+
+void setup(){
+    ads1115_1.begin();
+  Serial.begin(9600);
+} 
+void loop(){
+  float volts;
+  volts = voltage();
+  Serial.print("Voltage: ");
+  Serial.print(volts);
+  Serial.print(" V");
+  delay(1000);
+}
+
+float voltage (int n){
+    return ads1115_1.readADC_Differential_0_1()/0.0000762939453125;
+}
