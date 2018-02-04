bb10834d3109bca179a5831ceb35f6c2f0d35ab6
tiagomiotto <34488331+tiagomiotto@users.noreply.github.com>
2018-02-04 15:56:51 +0000

    Update potent_test_module.ino
    Updated in order  to send  the identification byte when the transmission starts, as stated on the datasheet

--- potent_test_module/potent_test_module.ino
+++ potent_test_module/potent_test_module.ino
@@ -2,20 +2,27 @@
 Potentiometer testing module
 The program is designed to increase the resistance of the potentiometer every 10 seconds
 allowing enough time to be measured confirming the proper functioning
+Assuming A1 and A0 at 5v -> In different cases change both the addr and the identification byte
+in the following form  
+addr: 10100(A1)(A0)
+identification: 10100(A1)(A0)0
 */
 
 #include <Wire.h>
 #include <math.h> 
 
-const int addr = 0x50;
-const int level = 0x100;
+const int addr = 0x53; //Addr
+byte level = 0x100;
+const byte ident = 0xA6
 
 void setup(){
   int aux_level=level;
   int resistance;
   Serial.begin(9600);
+  Wire.begin();
   Wire.beginTransmission(addr); // transmit to device #addr (0x2c)
   // device address is specified in datasheet
+  Wire.write(ident);   //identification byte
   Wire.write(byte(0x00));        // sends instruction byte // Essa instrução é pra escrever no Wiper Registry
   Wire.write(level);             // sends potentiometer value byte 
   Wire.endTransmission();     // stop transmitting
@@ -39,10 +46,11 @@ void loop(){
 
 void potentiometer(int level) 
 {
-  Wire.beginTransmission(addr); // transmit to device #addr (0x2c)
+  Wire.beginTransmission(addr); 
   // device address is specified in datasheet
+  Wire.write(ident);   //identification byte
   Wire.write(byte(0x00));        // sends instruction byte // Essa instrução é pra escrever no Wiper Registry
-  Wire.write(level);             // sends potentiometer value byte 
+  Wire.write(level);            // sends potentiometer value byte 
   Wire.endTransmission();     // stop transmitting
 }
 
