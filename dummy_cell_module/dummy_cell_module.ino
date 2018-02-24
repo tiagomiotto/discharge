2b0541f738f07b36fd2b5a8223dfb569136a6bb1
Dinis <denis.dr4@gmail.com>
2018-02-24 00:24:52 +0000

    Updated

--- dummy_cell_module/dummy_cell_module.ino
+++ dummy_cell_module/dummy_cell_module.ino
@@ -8,32 +8,32 @@ Current drain is expected to be around 14,38 due to the potentiometer's limitati
 #include <math.h> 
 
 /*Fill in the desired adc addr!*/
-Adafruit_ADS1115 ads1115_1(0x4B);
-const int rele_bat = 23;
+Adafruit_ADS1115 ads1115_1(0x48);
+const int rele_bat = 22;
+const int addr = 0x50;
+const int level=0xFF;
+const int termistor=A0;
+unsigned long tempo;
 
 void setup(){
   ads1115_1.begin();
   Serial.begin(9600);
   Wire.begin();
   pinMode(rele_bat,OUTPUT);
-  digitalWrite(rele_bat,HIGH);
-  Wire.beginTransmission(164); // transmit to device #addr (0xA4)
-  // device address is specified in datasheet
-  Wire.write(byte(0x00));        // sends instruction byte // Essa instrução é pra escrever no Wiper Registry
-  Wire.write(0x00);             // sends potentiometer value byte 
-  Wire.endTransmission();     // stop transmitting
+ 
+  potentiometer(level);
 } 
 void loop(){
-  float volts;
-  float curr;
+  float volts,curr,temp;
+  String data;
+  digitalWrite(rele_bat,LOW);
+  potentiometer(level);
   curr = current();
   volts = voltage();
-  Serial.print("Voltage: ");
-  Serial.print(volts);
-  Serial.println(" V");
-  Serial.print("Current: ");
-  Serial.print(curr);
-  Serial.println(" A");
+  temp=temperatures(termistor);
+  tempo=millis();
+  data = String(volts)+"V \t"+String(curr)+"A \t"+String(temp)+"C \t"+String(tempo)+"ms";
+  Serial.println(data);
   delay(1000);
 }
 
@@ -43,5 +43,24 @@ float voltage (){
 
 float current (){
     return (ads1115_1.readADC_Differential_0_1()*0.000188)/0.0003;
- }
+}
+void potentiometer(int lvl) 
+{
+  Wire.beginTransmission(addr); // transmit to device #addr (0x2c)
+  // device address is specified in datasheet
+
+  Wire.write(byte(0x00));        // sends instruction byte // Essa instrução é pra escrever no Wiper Registry
+  Wire.write(lvl);             // sends potentiometer value byte 
+  Wire.endTransmission();     // stop transmitting
+}
+
+float temperatures(int THERMISTORPIN){          
+  double reading;
+  reading = analogRead(THERMISTORPIN); 
+  float voltage = reading * (5.0 / 1023.0);
+  float current = voltage/10000;
+  float Rthermistor = (5-(1+125/10000)*voltage)/current;
+  float temperature = 3435/(log(Rthermistor/(10000*exp(-3435/298.15) ) )) - 273.15;
+  return  temperature;
+}
 
