1ec57a8bcc2eec6fb4dac87b239fd0c9f3703f99
tiagomiotto <34488331+tiagomiotto@users.noreply.github.com>
2018-04-04 19:25:22 +0100

    Updated and working

--- dummy_cell/src/main.cpp
+++ dummy_cell/src/main.cpp
@@ -10,21 +10,24 @@ Current drain is expected to be around 14,38 due to the potentiometer's limitati
 
 
 /*Fill in the desired adc addr!*/
-#define INA_ADDR 0x44
+#define CHG_INA 1
+#define DSC_INA 2
 #define POT_LEVEL 0x00
 #define POT_ADDR 0x53
 #define ADC_ADDR 0x48
 #define GPIO_ADDR 0x3B
-#define GPIO_PIN 7
-#define GPIO_PIN_2 6
+#define GPIO_BAT 7 //rele bat
+#define GPIO_CHG 6 //rele chg
 
 Adafruit_ADS1115 ads1115_1(0x4B);
-INA226_Class INA226; 
+INA226 ina; 
+INA226 ina_chg;
 TCA9534 gpio(0x38);
 const int addr = 0x50;
-const int level= 0x80;
+const int level= 0x00;
 const int termistor=A0;
 unsigned long tempo;
+
 
 void potentiometer(int);
 float temperatures(int);
@@ -35,50 +38,79 @@ void setup(){
   
   
   Wire.begin();
-  //ads1115_1.begin();
+  ads1115_1.begin();
   Serial.begin(9600);
  
   gpio.Begin();
-  gpio.PinMode(GPIO_PIN_2, OUTPUT);
-  gpio.PinMode(GPIO_PIN, OUTPUT); //colocar sempre essa merda
+  gpio.PinMode(GPIO_BAT, OUTPUT);
+  gpio.PinMode(GPIO_CHG, OUTPUT); //colocar sempre essa merda
+  gpio.PinMode(0, OUTPUT);
   gpio.PinMode(1, OUTPUT);
-  
-  INA226.begin(141,300,71); // o primeiro parametro é a corrente máxima do shunt da datasheet
+  gpio.PinMode(2, OUTPUT);
+  gpio.PinMode(3, OUTPUT);
+  gpio.PinMode(4, OUTPUT);
+  gpio.PinMode(5, OUTPUT);
 
-  INA226.setAveraging(4);                                                     // Average each reading n-times     //
-  INA226.setBusConversion(7);                                                 // Maximum conversion time 8.244ms  //
-  INA226.setShuntConversion(7);                                               // Maximum conversion time 8.244ms  //
-  INA226.setMode(INA_MODE_CONTINUOUS_BOTH); 
-  
-  //gpio.DigitalWrite(7,HIGH);
 
+  // o primeiro parametro é a corrente máxima do shunt da datasheet
+  // Default INA226 address is 0x40
+  ina.begin(0x46);
+  ina_chg.begin(0x40);
+  // Configure INA226
+  ina.configure(INA226_AVERAGES_1, INA226_BUS_CONV_TIME_1100US, INA226_SHUNT_CONV_TIME_1100US, INA226_MODE_SHUNT_BUS_CONT);
+
+  // Calibrate INA226. Rshunt = 0.0003 ohm, Corrente maxima do shunt 141.3
+  ina.calibrate(0.0003, 141.3);
+  ina_chg.configure(INA226_AVERAGES_1, INA226_BUS_CONV_TIME_1100US, INA226_SHUNT_CONV_TIME_1100US, INA226_MODE_SHUNT_BUS_CONT);
+
+  // Calibrate INA226. Rshunt = 0.001 ohm, Max accepted current = 71.2A
+  ina_chg.calibrate(0.001, 71.2);
 } 
+
 void loop(){
-  float volts=1,curr=1;
+
   String data;
   potentiometer(level);
-  curr = current()*-1;
-  //volts = voltage();
-  //temp=temperatures(termistor);
-  tempo=millis();
-  //data = String(volts)+"V \t"+String(curr)+"mA \t"+String(temp)+"C \t"+String(tempo)+"ms";
-  data = String(volts)+"V \t"+String(curr)+"mA \t"+String(tempo)+"ms";
-  Serial.println(data);
-  gpio.DigitalWrite(GPIO_PIN_2,LOW);
+  //Serial.println(data);
+  gpio.DigitalWrite(GPIO_BAT,LOW);
+  gpio.DigitalWrite(0,HIGH);
   gpio.DigitalWrite(1,HIGH);
+  gpio.DigitalWrite(2,HIGH);
+  gpio.DigitalWrite(3,HIGH);
+  /*
+  gpio.DigitalWrite(4,HIGH);
+  gpio.DigitalWrite(5,HIGH);
+  */
+  gpio.DigitalWrite(GPIO_CHG,HIGH);
+  
+  delay(500);
+  gpio.DigitalWrite(0,LOW);
+  gpio.DigitalWrite(1,LOW);
+  gpio.DigitalWrite(2,LOW);
+  gpio.DigitalWrite(3,LOW);
+  /*
+  gpio.DigitalWrite(4,LOW);
+  gpio.DigitalWrite(5,LOW); //Alert pins for inas
+  */
+  Serial.print("ADC Voltage ");
+  Serial.print(voltage(), 5);
+  Serial.println(" V");
 
-  gpio.DigitalWrite(GPIO_PIN,HIGH);
+  Serial.print("Shunt current: ");
+  Serial.print(ina.readShuntCurrent(), 5);
+  Serial.println(" A");
+  Serial.print("CHG Shunt current: ");
+  Serial.print(ina_chg.readShuntCurrent(), 5);
+  Serial.println(" A");
+
   delay(500);
 
 }
-/*
-float voltage (){
-    return ads1115_1.readADC_Differential_2_3()*0.000188;
-}*/
 
-float current (){
-    return (float)(INA226.getBusMicroAmps()/1000.0);
+float voltage (){
+    return ads1115_1.readADC_Differential_0_1()*0.000188*2;
 }
+
 
 void potentiometer(int lvl) 
 {
